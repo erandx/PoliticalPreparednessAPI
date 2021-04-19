82a7c9254d3690ebee6058dbac7895dbee4c829f
Erand Xhelilaj <xhelilaj.erand@gmail.com>
2021-04-19 11:23:59 -0400

    Setup CivicsApiService and Fixed App Crash

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -1,12 +1,19 @@
 package com.example.android.politicalpreparedness.election
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
+import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
+import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 
 class ElectionsFragment: Fragment() {
 
@@ -22,24 +29,42 @@ class ElectionsFragment: Fragment() {
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        val binding = FragmentElectionBinding.inflate(inflater)
+        // DONE: Add binding values
+        val binding : FragmentElectionBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_election, container, false)
+
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
 
-        //TODO: Add ViewModel values and create ViewModel
+        //Done: Link elections to voter info
+        viewModel.navigateToUpcomingElection.observe(viewLifecycleOwner, Observer { election ->
+            if(election != null)
+                this.findNavController().navigate(ElectionsFragmentDirections.actionElectionsFragmentToVoterInfoFragment(election))
+            viewModel.displayElectionDetailsComplete()
+            Log.d(TAG, "${election.id}, ${election.name}, ${election.division}")
+        })
 
-        //TODO: Add binding values
+        //Done: Initiate Election recycler + Followed recycler adapters
+        val electionAdapter = ElectionListAdapter(ElectionListAdapter.ElectionClickListener { election ->
+            viewModel.displayElectionDetails(election)
+        })
+        // HasStableIds will not cause any blinking in our UI
+        electionAdapter.setHasStableIds(true)
+        binding.recyclerUpcomingElections.adapter = electionAdapter
 
-        //TODO: Link elections to voter info
+        val savedAdapter = ElectionListAdapter(ElectionListAdapter.ElectionClickListener { savedElection ->
+        viewModel.displayElectionDetails(savedElection)
+        })
 
-        //TODO: Initiate recycler adapters
-
-        //TODO: Populate recycler adapters
+        savedAdapter.setHasStableIds(true)
+        binding.recyclerSavedElections.adapter = savedAdapter
 
         return binding.root
     }
 
     //TODO: Refresh adapters when fragment loads
 
+
 }
+private const val TAG = "ElectionsFragment"
--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -9,6 +9,7 @@ import com.example.android.politicalpreparedness.network.models.Election
 //TODO: Construct ViewModel and provide election datasource
 class ElectionsViewModel(application: Application): ViewModel() {
 
+
     //TODO: Create live data val for upcoming elections
     private val _upcomingElections = MutableLiveData<List<Election>>()
     val upcomingElections: LiveData<List<Election>>
@@ -22,5 +23,18 @@ class ElectionsViewModel(application: Application): ViewModel() {
     //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
 
     //TODO: Create functions to navigate to saved or upcoming election voter info
+    private val _navigateToUpcomingElection = MutableLiveData<Election>()
+    val navigateToUpcomingElection: LiveData<Election>
+    get() = _navigateToUpcomingElection
+
+    //Initiate navigation on Item Clicked
+    fun displayElectionDetails(election: Election){
+    _navigateToUpcomingElection.value = election
+    }
+
+    //We clean the LiveData to be triggered again after we return from Election Details
+    fun displayElectionDetailsComplete(){
+        _navigateToUpcomingElection.value = null
+    }
 
 }
